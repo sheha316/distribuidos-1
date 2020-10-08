8c775aa646c0154ec27f4d88cad26a7afca71c42
Nahelo97 <nataliahl1997@hotmail.com>
2020-10-08 18:41:38 -0300

    ya no es chitoso

--- comms/comms.go
+++ comms/comms.go
@@ -8,17 +8,16 @@ type Server struct {
 }
 
 
-var seguimiento int = 0
 
 func (s *Server) CrearOrdenPyme(ctx context.Context, request *Request_CrearOrdenPyme) (*Response_CrearOrden, error) {
   log.Printf("Receive message %s", request.id)
-  seguimiento += 1
+  seguimiento := 1
   return &Response_CrearOrden{Seguimiento: seguimiento}, nil
 }
 
 func (s *Server) CrearOrdenRetail(ctx context.Context, request *Request_CrearOrdenRetail) (*Response_CrearOrden, error) {
   log.Printf("Receive message %s", request.id)
-  seguimiento += 1
+  seguimiento := 1
   return &Response_CrearOrden{Seguimiento: seguimiento}, nil
 }
 
@@ -29,12 +28,8 @@ func (s *Server) Seguimiento(ctx context.Context, request *Request_Seguimiento) 
 
 func (s *Server) SolicitarPaquete(ctx context.Context, request *Request_SolicitarPaquete) (*Response_SolicitarPaquete, error) {
   log.Printf("Receive message %s", request.tipo)
-  return &Response_SolicitarPaquete{
-    Id: "vjdsv",
-    Tipo: "nvjnvkjf",
-    Valor: 7593,
-    Tienda: "dsbjd",
-    Destino: "bvjhdbvs"}, nil}
+  return &Response_SolicitarPaquete{Id: "vjdsv", Tipo: "nvjnvkjf", Valor: 7593, Tienda: "dsbjd", Destino: "bvjhdbvs"}, nil
+}
 
 func (s *Server) InformarEstado(ctx context.Context, request *Request_Estado) (*Response_Estado, error) {
   log.Printf("Receive message %s", request.id)
