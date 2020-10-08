7642041be9284042654de33a3aec03c2e4900166
root <root@dist93.inf.santiago.usm.cl>
2020-10-08 14:57:10 -0300

    could it be

--- test2/comms/comms.pb.go
+++ test2/comms/comms.pb.go
@@ -4,8 +4,12 @@
 package comms
 
 import (
+	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -117,4 +121,84 @@ var fileDescriptor_db39efb7717b7d47 = []byte{
 	0x0a, 0xe9, 0x72, 0x71, 0x04, 0x27, 0x56, 0x7a, 0xa4, 0xe6, 0xe4, 0xe4, 0x0b, 0xf1, 0xe9, 0x41,
 	0x2c, 0x86, 0x5a, 0x24, 0xc5, 0x0f, 0xe7, 0x43, 0x4c, 0x54, 0x62, 0x48, 0x62, 0x03, 0x3b, 0xca,
 	0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x33, 0xad, 0x58, 0x77, 0xa3, 0x00, 0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConnInterface
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion6
+
+// CommsClient is the client API for Comms service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type CommsClient interface {
+	SayHello(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+}
+
+type commsClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewCommsClient(cc grpc.ClientConnInterface) CommsClient {
+	return &commsClient{cc}
+}
+
+func (c *commsClient) SayHello(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/comms.Comms/SayHello", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// CommsServer is the server API for Comms service.
+type CommsServer interface {
+	SayHello(context.Context, *Request) (*Response, error)
+}
+
+// UnimplementedCommsServer can be embedded to have forward compatible implementations.
+type UnimplementedCommsServer struct {
+}
+
+func (*UnimplementedCommsServer) SayHello(ctx context.Context, req *Request) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SayHello not implemented")
+}
+
+func RegisterCommsServer(s *grpc.Server, srv CommsServer) {
+	s.RegisterService(&_Comms_serviceDesc, srv)
+}
+
+func _Comms_SayHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CommsServer).SayHello(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/comms.Comms/SayHello",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CommsServer).SayHello(ctx, req.(*Request))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Comms_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "comms.Comms",
+	HandlerType: (*CommsServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SayHello",
+			Handler:    _Comms_SayHello_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "comms.proto",
 }
