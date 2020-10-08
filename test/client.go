8cfa5a6dac9a855bae12510f1c9be3e0b9f8580f
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-08 00:41:58 -0300

    desesperacion

--- test/client.go
+++ test/client.go
@@ -9,7 +9,7 @@ import (
 
 func main(){
   var conn *grpc.ClientConn
-  conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+  conn, err := grpc.Dial("dist93:9000", grpc.WithInsecure())
   if err != nil {
     log.Fatalf("could not connect: %s", err)
   }
