a2f2eca837b8c7b3a93117ed8428b0733e0a09a0
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-09 15:52:44 -0300

    vamos a probar

--- comms/comms.go
+++ comms/comms.go
@@ -2,15 +2,38 @@ package comms
 import (
   "log"
   "golang.org/x/net/context"
+  "os"
+  "encoding/csv"
 )
 
 type Server struct {
 }
 
+string id = 1;
+string producto = 2;
+int32 valor = 3;
+string tienda = 4;
+string destino = 5;
+int32 prioritario = 6;
 
 func (s *Server) CrearOrdenPyme(ctx context.Context, request *Request_CrearOrdenPyme) (*Response_CrearOrden, error) {
   log.Printf("Receive message %s", request.Id)
-  seguimiento := int32(1)
+
+  seguimento:=0
+  file,erros:=os.Open("./paquetes/"+string(seguimento)+".csv")
+  while(erros==nil){
+    seguimento++
+    file,erros=os.Open("./paquetes/"+string(seguimento)+".csv")
+  }
+  file,erros:=os.Create("./paquetes/"+string(seguimento)+".csv")
+  if erros!=nil{
+    fmt.Println(erros)
+  }
+  writer:=csv.NewWriter(file)
+  var guardar = [][]string{
+    {request.Id,request.Producto,string(request.Valor),request.Tienda,request.Destino,string(request.Prioritario),}
+  }
+  erros=writer,WriteAll(guardar)
   return &Response_CrearOrden{Seguimiento: seguimiento}, nil
 }
 
