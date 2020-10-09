960dc5b59562ce6a2c2e274affa58e6e17a9cd45
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-08 21:32:38 -0300

    dlkaj

--- cliente.go
+++ cliente.go
@@ -8,6 +8,7 @@ import (
   "os"
   "io"
   "encoding/csv"
+  "strconv"
 )
 
 type Pedido_pymes struct{
@@ -32,10 +33,10 @@ func read_and_request_pymes(c grpc.ClientConnInterface){
     pedido_pymes=append(pedido_pymes,Pedido_pymes{
       Id:line[0],
       Producto:line[1],
-      Valor:int(line[2]),
+      Valor:strconv.Atoi(line[2])[0],
       Tienda:line[3],
       Destino:line[4],
-      Prioritario:int(line[5]),
+      Prioritario:strconv.Atoi(line[5])[0],
     })
   }
   for i:=0; i<len(pedido_pymes);i++{
@@ -62,7 +63,7 @@ func main() {
   }
   defer conn.Close()
   c := comms.NewCommsClient(conn)
-  read_and_request_pymes()
+  read_and_request_pymes(c)
 
   response, err := c.Seguimiento(context.Background(), &comms.Request_Seguimiento{Seguimiento: 1})
   if err != nil {
