9600071752cca8b2e630ffeeda8119e515874870
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-09 21:19:24 -0300

    gili-pollas

--- camion.go
+++ camion.go
@@ -40,5 +40,5 @@ func main() {
   }
   defer conn.Close()
   request_paquete(conn,camion_3)
-  log.Printf("Response from server: %d", int(camion_3.Id))
+  log.Printf("Response from server: %d", int(camion_3.Paquete_inf.Id))
 }
