dccd5ca32c62d24761f4613f1a7b4c3ef4412f64
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-11 19:10:50 -0300

    ya

--- Codigo/camion.go
+++ Codigo/camion.go
@@ -46,10 +46,10 @@ func request_paquete_2(conn *grpc.ClientConn, kamion *Camion){
   }
 }
 
-func cargar_camion(conn *grpc.ClientConn, kamion *Camion){
+func cargar_camion(conn *grpc.ClientConn, kamion *Camion,tiempo int){
   request_paquete_1(conn , kamion)
   if(kamion.Paquetes==1){
-    time.Sleep(3 * time.Second)
+    time.Sleep(tiempo * time.Second)
     request_paquete_2(conn , kamion)
     kamion.Estado=1
   }
@@ -68,11 +68,12 @@ func expirado(paquete paquete_info)(bool){
   return true
 }
 
-func Reparto(kamion *Camion){
+func Reparto(kamion *Camion,tiempo int){
   kamion.Estado=0
   if(kamion.Paquetes==2){
     if(kamion.Paquete_inf1.Valor>kamion.Paquete_inf2.Valor){
       if(!expirado(kamion.Paquete_inf1)){
+        time.Sleep(tiempo * time.Second)
         kamion.Estado=1
         kamion.Paquete_inf1.Intentos++
         if(rand.Intn(100)<80){
@@ -80,6 +81,7 @@ func Reparto(kamion *Camion){
         }
       }
       if(!expirado(kamion.Paquete_inf2)){
+        time.Sleep(tiempo * time.Second)
         kamion.Paquete_inf2.Intentos++
         kamion.Estado=1
         if(rand.Intn(100)<80){
@@ -88,6 +90,7 @@ func Reparto(kamion *Camion){
       }
     }else if(kamion.Paquete_inf2.Valor>=kamion.Paquete_inf1.Valor){
       if(!expirado(kamion.Paquete_inf2)){
+        time.Sleep(tiempo * time.Second)
         kamion.Paquete_inf2.Intentos++
         kamion.Estado=1
         if(rand.Intn(100)<80){
@@ -95,6 +98,7 @@ func Reparto(kamion *Camion){
         }
       }
       if(!expirado(kamion.Paquete_inf1)){
+        time.Sleep(tiempo * time.Second)
         kamion.Paquete_inf1.Intentos++
         kamion.Estado=1
         if(rand.Intn(100)<80){
@@ -104,6 +108,7 @@ func Reparto(kamion *Camion){
     }
   }else if(kamion.Paquetes==1){
     if(!expirado(kamion.Paquete_inf1)){
+      time.Sleep(tiempo * time.Second)
       kamion.Paquete_inf1.Intentos++
       kamion.Estado=1
       if(rand.Intn(100)<80){
@@ -139,6 +144,7 @@ func reporte(conn *grpc.ClientConn,kamion *Camion){
     kamion.Paquetes=0
   }
 }
+
 func superprint_fs(kamion *Camion){
   log.Printf("Camion: "+kamion.Id+",Tipo:"+kamion.Tipo+",Paquetes:"+strconv.Itoa(kamion.Paquetes))
   if(kamion.Paquetes==1){
@@ -171,23 +177,24 @@ func main() {
     log.Fatalf("did not connect: %s", err)
   }
   defer conn.Close()
-  var input_us string
-  input_us=""
-  for input_us!="0"{
-    log.Printf("Bienvenido! ingrese el numero de la opcion que desea")
-    log.Printf("1-Hacer entrega")
-    log.Printf("0-exit")
-    fmt.Scanln(&input_us)
-    switch input_us {
-    case "1":
+  var T_sp int
+  var T_ep int
+  var T_f int
+  log.Printf("Tiempo de espera de 2do paquete")
+  fmt.Scanln(&T_sp)
+  log.Printf("Tiempo que demora en entregar un paquete")
+  fmt.Scanln(&T_ep)
+  log.Printf("Tiempo entre envio")
+  fmt.Scanln(&T_f)
+  for{
       if(camion_1.Estado==0){
-        cargar_camion(conn,camion_1)
+        cargar_camion(conn,camion_1,T_sp)
       }
       if(camion_2.Estado==0){
-        cargar_camion(conn,camion_2)
+        cargar_camion(conn,camion_2,T_sp)
       }
       if(camion_3.Estado==0){
-        cargar_camion(conn,camion_3)
+        cargar_camion(conn,camion_3,T_sp)
       }
       log.Printf("----Paquetes Recibidos----")
       superprint_fs(camion_1)
@@ -195,13 +202,13 @@ func main() {
       superprint_fs(camion_3)
       log.Printf("--------------------------")/**/
       for camion_1.Estado==1{
-        Reparto(camion_1)
+        Reparto(camion_1,T_ep)
       }
       for camion_2.Estado==1{
-        Reparto(camion_2)
+        Reparto(camion_2,T_ep)
       }
       for camion_3.Estado==1{
-        Reparto(camion_3)
+        Reparto(camion_3,T_ep)
       }
       log.Printf("----Paquetes Entrgados----")
       superprint_ts(camion_1)
@@ -217,7 +224,6 @@ func main() {
       if(camion_3.Paquetes!=0){
         reporte(conn,camion_3)
       }
-    default:
+      time.Sleep(T_f * time.Second)
     }
-  }
 }
