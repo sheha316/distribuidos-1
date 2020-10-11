6dcc2db7f163a03cc270a704f4283705d29e3f53
root <root@dist93.inf.santiago.usm.cl>
2020-10-11 11:50:09 -0300

    wea

--- Codigo/comms/comms.pb.go
+++ Codigo/comms/comms.pb.go
@@ -344,6 +344,7 @@ type Response_SolicitarPaquete struct {
 	Tipo                 string   `protobuf:"bytes,3,opt,name=tipo,proto3" json:"tipo,omitempty"`
 	Valor                int32    `protobuf:"varint,4,opt,name=valor,proto3" json:"valor,omitempty"`
 	Tienda               string   `protobuf:"bytes,5,opt,name=tienda,proto3" json:"tienda,omitempty"`
+	Destino              string   `protobuf:"bytes,6,opt,name=destino,proto3" json:"destino,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -405,6 +406,13 @@ func (m *Response_SolicitarPaquete) GetValor() int32 {
 func (m *Response_SolicitarPaquete) GetTienda() string {
 	if m != nil {
 		return m.Tienda
+	}
+	return ""
+}
+
+func (m *Response_SolicitarPaquete) GetDestino() string {
+	if m != nil {
+		return m.Destino
 	}
 	return ""
 }
@@ -528,36 +536,36 @@ func init() {
 }
 
 var fileDescriptor_db39efb7717b7d47 = []byte{
-	// 460 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xc4, 0x54, 0xc1, 0x8e, 0xd3, 0x30,
-	0x14, 0xdc, 0xb4, 0x4d, 0x80, 0x57, 0x29, 0xac, 0xcc, 0x52, 0x99, 0x20, 0x44, 0xe5, 0x13, 0x17,
-	0x7a, 0x80, 0x03, 0x37, 0x24, 0x84, 0x90, 0x40, 0x02, 0xb1, 0x64, 0x0f, 0x1c, 0x91, 0x37, 0xf1,
-	0x16, 0xa3, 0x26, 0x0e, 0x8e, 0x8b, 0xc4, 0x4f, 0x00, 0x5f, 0xc2, 0x37, 0xe2, 0xd8, 0x8e, 0x6b,
-	0x9c, 0x22, 0xb8, 0x71, 0x6a, 0xe6, 0xd9, 0x6f, 0x66, 0xde, 0xd8, 0x2e, 0x2c, 0x2b, 0xd1, 0x34,
-	0xfd, 0xa6, 0x93, 0x42, 0x09, 0x94, 0x1a, 0x40, 0x7e, 0x26, 0xb0, 0x2a, 0xd9, 0xe7, 0x3d, 0xeb,
-	0xd5, 0x87, 0xe7, 0x92, 0x51, 0xf9, 0x56, 0xd6, 0xac, 0x3d, 0xff, 0xda, 0x30, 0x94, 0xc3, 0x8c,
-	0xd7, 0x38, 0x59, 0x27, 0x0f, 0x6e, 0x94, 0xfa, 0x0b, 0x15, 0x70, 0x5d, 0xb7, 0xd6, 0xfb, 0x4a,
-	0x09, 0x3c, 0x33, 0x55, 0x8f, 0xd1, 0x19, 0xa4, 0x5f, 0xe8, 0x4e, 0x48, 0x3c, 0xd7, 0x0b, 0x69,
-	0x69, 0x01, 0x5a, 0x41, 0xa6, 0x38, 0x6b, 0x6b, 0x8a, 0x17, 0x66, 0xbf, 0x43, 0x08, 0xc3, 0xb5,
-	0x5a, 0x0b, 0xf2, 0x56, 0xe0, 0xd4, 0x2c, 0x8c, 0x10, 0xad, 0x61, 0xd9, 0x49, 0x2e, 0x24, 0x57,
-	0x54, 0xff, 0xe0, 0xcc, 0xb0, 0x85, 0x25, 0xf2, 0x2d, 0x01, 0x3c, 0x35, 0x5c, 0x32, 0x45, 0xf9,
-	0xee, 0x7f, 0x58, 0x26, 0x4f, 0xe0, 0x56, 0xc9, 0xfa, 0x4e, 0xb4, 0x3d, 0x0b, 0x0c, 0x0d, 0x93,
-	0xf4, 0x6c, 0xbb, 0xe7, 0x8d, 0xee, 0xd7, 0xea, 0x89, 0x9d, 0x24, 0x28, 0xd9, 0x46, 0x3b, 0xc8,
-	0xc5, 0xa1, 0xfc, 0x0f, 0x8d, 0x1b, 0x38, 0xf3, 0x8a, 0x61, 0xa7, 0xf6, 0xae, 0xc9, 0x68, 0x2d,
-	0x5c, 0x02, 0x0e, 0x91, 0xa7, 0x87, 0xc4, 0x2e, 0xc4, 0x8e, 0x57, 0x43, 0x92, 0xe7, 0x54, 0x17,
-	0x14, 0x43, 0x08, 0x16, 0x8a, 0x77, 0x63, 0x87, 0xf9, 0x76, 0x29, 0xce, 0xc6, 0x14, 0xc9, 0xf7,
-	0x04, 0xee, 0x1c, 0x04, 0x63, 0x86, 0x38, 0xf3, 0xc8, 0xff, 0x6c, 0xe2, 0xdf, 0x6b, 0xce, 0x03,
-	0x4d, 0x7f, 0x1a, 0x8b, 0xe3, 0xa7, 0x91, 0x86, 0xa7, 0x41, 0x3e, 0x41, 0x3e, 0x4e, 0xf4, 0xc2,
-	0xcc, 0x78, 0xec, 0xe4, 0x79, 0xab, 0x06, 0xb9, 0xde, 0x59, 0xf0, 0x78, 0xd0, 0xba, 0x62, 0xd5,
-	0x47, 0xea, 0x0c, 0x58, 0x10, 0xa4, 0xb7, 0xf8, 0x2d, 0xbd, 0x87, 0x70, 0xd3, 0x0f, 0xef, 0xc4,
-	0x34, 0xb9, 0x64, 0x15, 0xbf, 0xe4, 0x3e, 0x6a, 0x8f, 0x1f, 0xfd, 0x98, 0x43, 0xf6, 0x5a, 0x6c,
-	0x79, 0x45, 0xd1, 0x1b, 0xc8, 0xa3, 0x27, 0x75, 0x6f, 0x63, 0x9f, 0xe0, 0xf1, 0x17, 0x57, 0x14,
-	0x7e, 0x79, 0x72, 0x9f, 0xc8, 0x09, 0x7a, 0x07, 0xa7, 0x93, 0x0b, 0x7f, 0xff, 0x8f, 0x84, 0x76,
-	0xc3, 0x5f, 0x28, 0x5f, 0xc2, 0x32, 0xbc, 0x40, 0x45, 0xc4, 0x16, 0xac, 0x15, 0x77, 0x63, 0xa2,
-	0x60, 0x51, 0x33, 0xbd, 0x87, 0xd3, 0xc9, 0xcd, 0x88, 0xcd, 0xc5, 0x1b, 0x8a, 0xf5, 0x84, 0x33,
-	0xda, 0xa1, 0x89, 0x9f, 0x41, 0xfe, 0xaa, 0xbd, 0x12, 0xb2, 0xa1, 0xd2, 0xa5, 0x7f, 0x3b, 0xa2,
-	0xb5, 0xe5, 0x62, 0x15, 0x93, 0xd9, 0x3a, 0x39, 0xb9, 0xcc, 0xcc, 0x3f, 0xde, 0xe3, 0x5f, 0x01,
-	0x00, 0x00, 0xff, 0xff, 0xd4, 0x42, 0xdd, 0xfa, 0x00, 0x05, 0x00, 0x00,
+	// 464 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xc4, 0x54, 0xb1, 0x8e, 0xd4, 0x30,
+	0x10, 0xbd, 0x64, 0x93, 0x00, 0xb3, 0x52, 0x38, 0x99, 0x63, 0x65, 0x82, 0x10, 0xab, 0x54, 0x34,
+	0x6c, 0x01, 0x05, 0x1d, 0x12, 0x3a, 0x21, 0x41, 0x81, 0x38, 0x72, 0x05, 0x25, 0xf2, 0x25, 0x3e,
+	0x30, 0xda, 0xc4, 0xc1, 0xf1, 0x22, 0xf1, 0x13, 0xf0, 0x1d, 0x34, 0x7c, 0x23, 0x8e, 0xed, 0x78,
+	0x7d, 0xce, 0x9e, 0xa0, 0xbb, 0x6a, 0xf3, 0xc6, 0x9e, 0x37, 0x6f, 0xde, 0x78, 0x16, 0x96, 0x35,
+	0x6f, 0xdb, 0x61, 0xd3, 0x0b, 0x2e, 0x39, 0x4a, 0x35, 0x28, 0xff, 0x44, 0xb0, 0xaa, 0xe8, 0xb7,
+	0x1d, 0x1d, 0xe4, 0xa7, 0x53, 0x41, 0x89, 0x78, 0x2f, 0x1a, 0xda, 0x9d, 0xfd, 0x68, 0x29, 0xca,
+	0x21, 0x66, 0x0d, 0x8e, 0xd6, 0xd1, 0x93, 0x3b, 0x95, 0xfa, 0x42, 0x05, 0xdc, 0x56, 0xa9, 0xcd,
+	0xae, 0x96, 0x1c, 0xc7, 0x3a, 0xea, 0x30, 0x3a, 0x81, 0xf4, 0x3b, 0xd9, 0x72, 0x81, 0x17, 0xea,
+	0x20, 0xad, 0x0c, 0x40, 0x2b, 0xc8, 0x24, 0xa3, 0x5d, 0x43, 0x70, 0xa2, 0xef, 0x5b, 0x84, 0x30,
+	0xdc, 0x6a, 0x54, 0x41, 0xd6, 0x71, 0x9c, 0xea, 0x83, 0x09, 0xa2, 0x35, 0x2c, 0x7b, 0xc1, 0xb8,
+	0x60, 0x92, 0xa8, 0x1f, 0x9c, 0x69, 0x36, 0x3f, 0x54, 0xfe, 0x8c, 0x00, 0xcf, 0x05, 0x57, 0x54,
+	0x12, 0xb6, 0xbd, 0x09, 0xc9, 0xe5, 0x0b, 0xb8, 0x57, 0xd1, 0xa1, 0xe7, 0xdd, 0x40, 0x3d, 0x41,
+	0x63, 0x27, 0x03, 0xfd, 0xbc, 0x63, 0xad, 0xca, 0x57, 0xd5, 0x23, 0xd3, 0x89, 0x17, 0x32, 0x89,
+	0xa6, 0x91, 0xf3, 0x7d, 0xf8, 0x3f, 0x12, 0x37, 0x70, 0xe2, 0x2a, 0xfa, 0x99, 0x4a, 0xbb, 0x22,
+	0x23, 0x0d, 0xb7, 0x0e, 0x58, 0x54, 0xbe, 0xdc, 0x3b, 0x76, 0xce, 0xb7, 0xac, 0x1e, 0x9d, 0x3c,
+	0x23, 0x2a, 0x20, 0x29, 0x42, 0x90, 0x48, 0xd6, 0x4f, 0x19, 0xfa, 0xdb, 0xba, 0x18, 0x4f, 0x2e,
+	0x96, 0xbf, 0x23, 0x78, 0xb0, 0x2f, 0x18, 0x32, 0x84, 0x9e, 0x07, 0xfa, 0xe3, 0x99, 0x7e, 0x57,
+	0x73, 0xe1, 0xd5, 0x74, 0xd3, 0x48, 0x0e, 0x4f, 0x23, 0xbd, 0x6e, 0x1a, 0xd9, 0xd5, 0x69, 0x7c,
+	0x85, 0x7c, 0xea, 0xf5, 0xb5, 0xee, 0xfe, 0xd0, 0x9b, 0x60, 0x9d, 0x1c, 0x85, 0x0c, 0x56, 0x9c,
+	0xc3, 0xa3, 0x8a, 0x4b, 0x5a, 0x7f, 0x21, 0x56, 0x9a, 0x01, 0x9e, 0xaf, 0xc9, 0x15, 0x5f, 0x9f,
+	0xc2, 0x5d, 0x67, 0x8b, 0x2d, 0xa6, 0xc8, 0x05, 0xad, 0xd9, 0x05, 0x73, 0x43, 0x70, 0xf8, 0xd9,
+	0xaf, 0x05, 0xa4, 0xa7, 0xe3, 0xd2, 0xa1, 0x77, 0x90, 0x07, 0xbb, 0xf6, 0x68, 0x63, 0x76, 0xf3,
+	0xf0, 0x2a, 0x16, 0x85, 0x3b, 0x9e, 0x3d, 0xb4, 0xf2, 0x08, 0x7d, 0x80, 0xe3, 0xd9, 0x26, 0x3c,
+	0xbe, 0x96, 0xd0, 0x5c, 0xf8, 0x07, 0xe5, 0x1b, 0x58, 0xfa, 0x2f, 0xab, 0x08, 0xd8, 0xbc, 0xb3,
+	0xe2, 0x61, 0x48, 0xe4, 0x1d, 0x2a, 0xa6, 0x8f, 0x70, 0x3c, 0x7b, 0x32, 0xa1, 0xb8, 0xf0, 0x42,
+	0xb1, 0x9e, 0x71, 0x06, 0x37, 0x14, 0xf1, 0x2b, 0xc8, 0xdf, 0x76, 0x97, 0x5c, 0xb4, 0x44, 0x58,
+	0xf3, 0xef, 0x07, 0xb4, 0x26, 0x5c, 0xac, 0x42, 0x32, 0x13, 0x2f, 0x8f, 0x2e, 0x32, 0xfd, 0x57,
+	0xf8, 0xfc, 0x6f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x7f, 0xb8, 0xf8, 0xc5, 0x19, 0x05, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -568,10 +576,10 @@ var _ grpc.ClientConnInterface
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion6
 
-// LogicaClient is the client API for Logica service.
+// CommsClient is the client API for Comms service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type LogicaClient interface {
+type CommsClient interface {
 	CrearOrdenPyme(ctx context.Context, in *Request_CrearOrdenPyme, opts ...grpc.CallOption) (*Response_CrearOrden, error)
 	CrearOrdenRetail(ctx context.Context, in *Request_CrearOrdenRetail, opts ...grpc.CallOption) (*Response_CrearOrden, error)
 	Seguimiento(ctx context.Context, in *Request_Seguimiento, opts ...grpc.CallOption) (*Response_Seguimiento, error)
@@ -579,61 +587,61 @@ type LogicaClient interface {
 	InformarEstado(ctx context.Context, in *Request_Estado, opts ...grpc.CallOption) (*Response_Estado, error)
 }
 
-type logicaClient struct {
+type commsClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewLogicaClient(cc grpc.ClientConnInterface) LogicaClient {
-	return &logicaClient{cc}
+func NewCommsClient(cc grpc.ClientConnInterface) CommsClient {
+	return &commsClient{cc}
 }
 
-func (c *logicaClient) CrearOrdenPyme(ctx context.Context, in *Request_CrearOrdenPyme, opts ...grpc.CallOption) (*Response_CrearOrden, error) {
+func (c *commsClient) CrearOrdenPyme(ctx context.Context, in *Request_CrearOrdenPyme, opts ...grpc.CallOption) (*Response_CrearOrden, error) {
 	out := new(Response_CrearOrden)
-	err := c.cc.Invoke(ctx, "/comms.Logica/CrearOrdenPyme", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/comms.Comms/CrearOrdenPyme", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logicaClient) CrearOrdenRetail(ctx context.Context, in *Request_CrearOrdenRetail, opts ...grpc.CallOption) (*Response_CrearOrden, error) {
+func (c *commsClient) CrearOrdenRetail(ctx context.Context, in *Request_CrearOrdenRetail, opts ...grpc.CallOption) (*Response_CrearOrden, error) {
 	out := new(Response_CrearOrden)
-	err := c.cc.Invoke(ctx, "/comms.Logica/CrearOrdenRetail", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/comms.Comms/CrearOrdenRetail", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logicaClient) Seguimiento(ctx context.Context, in *Request_Seguimiento, opts ...grpc.CallOption) (*Response_Seguimiento, error) {
+func (c *commsClient) Seguimiento(ctx context.Context, in *Request_Seguimiento, opts ...grpc.CallOption) (*Response_Seguimiento, error) {
 	out := new(Response_Seguimiento)
-	err := c.cc.Invoke(ctx, "/comms.Logica/Seguimiento", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/comms.Comms/Seguimiento", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logicaClient) SolicitarPaquete(ctx context.Context, in *Request_SolicitarPaquete, opts ...grpc.CallOption) (*Response_SolicitarPaquete, error) {
+func (c *commsClient) SolicitarPaquete(ctx context.Context, in *Request_SolicitarPaquete, opts ...grpc.CallOption) (*Response_SolicitarPaquete, error) {
 	out := new(Response_SolicitarPaquete)
-	err := c.cc.Invoke(ctx, "/comms.Logica/SolicitarPaquete", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/comms.Comms/SolicitarPaquete", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *logicaClient) InformarEstado(ctx context.Context, in *Request_Estado, opts ...grpc.CallOption) (*Response_Estado, error) {
+func (c *commsClient) InformarEstado(ctx context.Context, in *Request_Estado, opts ...grpc.CallOption) (*Response_Estado, error) {
 	out := new(Response_Estado)
-	err := c.cc.Invoke(ctx, "/comms.Logica/InformarEstado", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/comms.Comms/InformarEstado", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// LogicaServer is the server API for Logica service.
-type LogicaServer interface {
+// CommsServer is the server API for Comms service.
+type CommsServer interface {
 	CrearOrdenPyme(context.Context, *Request_CrearOrdenPyme) (*Response_CrearOrden, error)
 	CrearOrdenRetail(context.Context, *Request_CrearOrdenRetail) (*Response_CrearOrden, error)
 	Seguimiento(context.Context, *Request_Seguimiento) (*Response_Seguimiento, error)
@@ -641,143 +649,143 @@ type LogicaServer interface {
 	InformarEstado(context.Context, *Request_Estado) (*Response_Estado, error)
 }
 
-// UnimplementedLogicaServer can be embedded to have forward compatible implementations.
-type UnimplementedLogicaServer struct {
+// UnimplementedCommsServer can be embedded to have forward compatible implementations.
+type UnimplementedCommsServer struct {
 }
 
-func (*UnimplementedLogicaServer) CrearOrdenPyme(ctx context.Context, req *Request_CrearOrdenPyme) (*Response_CrearOrden, error) {
+func (*UnimplementedCommsServer) CrearOrdenPyme(ctx context.Context, req *Request_CrearOrdenPyme) (*Response_CrearOrden, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CrearOrdenPyme not implemented")
 }
-func (*UnimplementedLogicaServer) CrearOrdenRetail(ctx context.Context, req *Request_CrearOrdenRetail) (*Response_CrearOrden, error) {
+func (*UnimplementedCommsServer) CrearOrdenRetail(ctx context.Context, req *Request_CrearOrdenRetail) (*Response_CrearOrden, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CrearOrdenRetail not implemented")
 }
-func (*UnimplementedLogicaServer) Seguimiento(ctx context.Context, req *Request_Seguimiento) (*Response_Seguimiento, error) {
+func (*UnimplementedCommsServer) Seguimiento(ctx context.Context, req *Request_Seguimiento) (*Response_Seguimiento, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Seguimiento not implemented")
 }
-func (*UnimplementedLogicaServer) SolicitarPaquete(ctx context.Context, req *Request_SolicitarPaquete) (*Response_SolicitarPaquete, error) {
+func (*UnimplementedCommsServer) SolicitarPaquete(ctx context.Context, req *Request_SolicitarPaquete) (*Response_SolicitarPaquete, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SolicitarPaquete not implemented")
 }
-func (*UnimplementedLogicaServer) InformarEstado(ctx context.Context, req *Request_Estado) (*Response_Estado, error) {
+func (*UnimplementedCommsServer) InformarEstado(ctx context.Context, req *Request_Estado) (*Response_Estado, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method InformarEstado not implemented")
 }
 
-func RegisterLogicaServer(s *grpc.Server, srv LogicaServer) {
-	s.RegisterService(&_Logica_serviceDesc, srv)
+func RegisterCommsServer(s *grpc.Server, srv CommsServer) {
+	s.RegisterService(&_Comms_serviceDesc, srv)
 }
 
-func _Logica_CrearOrdenPyme_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Comms_CrearOrdenPyme_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request_CrearOrdenPyme)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogicaServer).CrearOrdenPyme(ctx, in)
+		return srv.(CommsServer).CrearOrdenPyme(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/comms.Logica/CrearOrdenPyme",
+		FullMethod: "/comms.Comms/CrearOrdenPyme",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogicaServer).CrearOrdenPyme(ctx, req.(*Request_CrearOrdenPyme))
+		return srv.(CommsServer).CrearOrdenPyme(ctx, req.(*Request_CrearOrdenPyme))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Logica_CrearOrdenRetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Comms_CrearOrdenRetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request_CrearOrdenRetail)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogicaServer).CrearOrdenRetail(ctx, in)
+		return srv.(CommsServer).CrearOrdenRetail(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/comms.Logica/CrearOrdenRetail",
+		FullMethod: "/comms.Comms/CrearOrdenRetail",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogicaServer).CrearOrdenRetail(ctx, req.(*Request_CrearOrdenRetail))
+		return srv.(CommsServer).CrearOrdenRetail(ctx, req.(*Request_CrearOrdenRetail))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Logica_Seguimiento_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Comms_Seguimiento_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request_Seguimiento)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogicaServer).Seguimiento(ctx, in)
+		return srv.(CommsServer).Seguimiento(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/comms.Logica/Seguimiento",
+		FullMethod: "/comms.Comms/Seguimiento",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogicaServer).Seguimiento(ctx, req.(*Request_Seguimiento))
+		return srv.(CommsServer).Seguimiento(ctx, req.(*Request_Seguimiento))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Logica_SolicitarPaquete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Comms_SolicitarPaquete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request_SolicitarPaquete)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogicaServer).SolicitarPaquete(ctx, in)
+		return srv.(CommsServer).SolicitarPaquete(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/comms.Logica/SolicitarPaquete",
+		FullMethod: "/comms.Comms/SolicitarPaquete",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogicaServer).SolicitarPaquete(ctx, req.(*Request_SolicitarPaquete))
+		return srv.(CommsServer).SolicitarPaquete(ctx, req.(*Request_SolicitarPaquete))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Logica_InformarEstado_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Comms_InformarEstado_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request_Estado)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(LogicaServer).InformarEstado(ctx, in)
+		return srv.(CommsServer).InformarEstado(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/comms.Logica/InformarEstado",
+		FullMethod: "/comms.Comms/InformarEstado",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(LogicaServer).InformarEstado(ctx, req.(*Request_Estado))
+		return srv.(CommsServer).InformarEstado(ctx, req.(*Request_Estado))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Logica_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "comms.Logica",
-	HandlerType: (*LogicaServer)(nil),
+var _Comms_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "comms.Comms",
+	HandlerType: (*CommsServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "CrearOrdenPyme",
-			Handler:    _Logica_CrearOrdenPyme_Handler,
+			Handler:    _Comms_CrearOrdenPyme_Handler,
 		},
 		{
 			MethodName: "CrearOrdenRetail",
-			Handler:    _Logica_CrearOrdenRetail_Handler,
+			Handler:    _Comms_CrearOrdenRetail_Handler,
 		},
 		{
 			MethodName: "Seguimiento",
-			Handler:    _Logica_Seguimiento_Handler,
+			Handler:    _Comms_Seguimiento_Handler,
 		},
 		{
 			MethodName: "SolicitarPaquete",
-			Handler:    _Logica_SolicitarPaquete_Handler,
+			Handler:    _Comms_SolicitarPaquete_Handler,
 		},
 		{
 			MethodName: "InformarEstado",
-			Handler:    _Logica_InformarEstado_Handler,
+			Handler:    _Comms_InformarEstado_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
