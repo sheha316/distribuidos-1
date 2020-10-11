654db3c4caaf888109733ebb72dfdab73ed337e6
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-11 16:20:14 -0300

    a ver

--- Codigo/logica.go
+++ Codigo/logica.go
@@ -238,6 +238,7 @@ func Updater(n_file string,estado string){
   csvfile ,_= os.Open(nombrearch)
   reader = csv.NewReader(bufio.NewReader(csvfile))
   log.Printf("pica3")
+  _,_:=os.Remove("../storage/logica/aux.csv")
   csvfilex ,_:= os.OpenFile("../storage/logica/aux.csv", os.O_WRONLY|os.O_CREATE, 0777)
   writer:=csv.NewWriter(csvfilex)
   for{
