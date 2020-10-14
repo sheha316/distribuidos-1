e36c555933c1cec2eb1142163f8709b1c77d725c
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-13 22:43:38 -0300

    wea

--- Codigo/finanzas.go
+++ Codigo/finanzas.go
@@ -79,13 +79,13 @@ func main() {
 		ganancia:=0
 		if(fech!="0"){
 			perdida-=1
-			ganancia=strconv.Atoi(valor)
+			ganancia,_=strconv.Atoi(valor)
 
 		}else{
 			if(tipo=="retail"){
-				ganancia=strconv.Atoi(valor)
+				ganancia,_=strconv.Atoi(valor)
 			}else if(tipo=="prioritario"){
-				ganancia=strconv.Atoi(valor)*0.3
+				ganancia,_=strconv.Atoi(valor)*0.3
 			}
 		}
 		perdida*=10
