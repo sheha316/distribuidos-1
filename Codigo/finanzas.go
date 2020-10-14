d1e489661737f550964d0cb5968131ed6f1e677e
ashehadeh <andres.shehdeh@sansano.usm.cl>
2020-10-13 21:07:12 -0300

    n,

--- Codigo/finanzas.go
+++ Codigo/finanzas.go
@@ -48,50 +48,27 @@ func main() {
 			log.Printf("Received a message: %s", d.Body)
 			file, err := os.Create("result.csv")
 	    checkError("Cannot create file", err)
-	    defer file.Close()
-
-	    writer := csv.NewWriter(file)
-	    defer writer.Flush()
-
-			var data = [][]string{{"Line1", "Hello Readers of"}, {"Line2", "golangcode.com"}}
-
-	    for _, value := range data {
-	        err := writer.Write(value)
-	        checkError("Cannot write to file", err)
-	    }
-			Finances()
-
+			Finances(d.Body)
 		}
+	}
 
 	}()
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
 
 }
-	func Finances() {
-		// Open the file
-		csvfile, err := os.Open("result.csv")
+	func Finances(body string) {
+		f, err := os.OpenFile("../storage/finanzas/result.csv", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 		if err != nil {
 			log.Fatalln("Couldn't open the csv file", err)
 		}
-
-		// Parse the file
-		r := csv.NewReader(csvfile)
-		//r := csv.NewReader(bufio.NewReader(csvfile))
-
-		// Iterate through the records
-		for {
-			// Read each record from csv
-			record, err := r.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("cosa: %s \n", record)
-		}
-	}
+		defer f.Close()
+		var data [][]string
+	  data = append(data, []string{body})
+	  w := csv.NewWriter(f)
+		w.WriteAll(data)
+		f.Close()
+}
 
 	func checkError(message string, err error) {
 	    if err != nil {
